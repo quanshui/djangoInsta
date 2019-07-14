107fb56c4f1e0184df241dbf561a59fe0915b98e
qichuan <qichuanbai.com>
2019-07-14 19:13:53 -0400

    add comment/like

--- myInsta/admin.py
+++ myInsta/admin.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from myInsta.models import Post, InstaUser
+from myInsta.models import Post, InstaUser, Comment, Like
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(InstaUser)
+admin.site.register(Comment)
+admin.site.register(Like)
--- myInsta/models.py
+++ myInsta/models.py
@@ -8,7 +8,24 @@ from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class InstaUser(AbstractUser):
+    profile_pic=ProcessedImageField(
+        upload_to = 'static/images/profiles',
+        format = 'JPEG',
+        blank = True,
+        null = True,
+        )
+
 class Post(models.Model):
+    author = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        blank = True,
+        null = True,
+    )
+
     title = models.TextField(blank=True, null=True)
     image = ProcessedImageField(
         upload_to = 'static/images/posts',
@@ -22,11 +39,37 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse("detail", args=[str(self.id)])
-    
-class InstaUser(AbstractUser):
-    profile_pic=ProcessedImageField(
-        upload_to = 'static/images/profiles',
-        format = 'JPEG',
-        blank = True,
-        null = True,
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='comments',
         )
+    comment = models.CharField(max_length=140)
+    posted_on = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+    def __str__(self):
+        return self.comment
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes' #external ref to join table
+    )
+    
+    def __str__(self):
+        return 'Likes: ' + self.user.username + ' likes '
